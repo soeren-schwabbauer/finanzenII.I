e589712a6242cf0e82886ffe7da7139313539496
soeren-schwabbauer <soeren@schwabbauer.de>
2024-12-05 20:25:38 +0100

    switch to comments in csv file

--- app.R
+++ app.R
@@ -22,59 +22,43 @@ source("./auswertunggiro_mod.R")
 source("./upload.R")
 
 ################################################################################
-# KONTEN #######################################################################
-# Create the bank and konto combinations
+# LOAD DATA ####################################################################
 
+# finanzkonten file names - are IDentifier for each konto
+filenames_csv <- list.files(path = "./DATA/")
+filenames_csv <- filenames_csv[filenames_csv != "gruppen.csv"]
 
-# Create the dataframe using tribble with bank, konto, name, and ID columns
-bank_konto <- tribble(
-  ~bank,           ~konto,        ~name,                    ~ID,
-  "ING",           "GIRO",        "ING - GIRO",             "INGDDEFFXXX_GIR",
-  "ING",           "DEPOT",       "ING - DEPOT",            "INGDDEFFXXX_DEP",
-  "ING",           "SPARBRIEF",   "ING - SPARBRIEF",        "INGDDEFFXXX_SPB",
-  "ING",           "EXTRAKONTO",  "ING - EXTRAKONTO",       "INGDDEFFXXX_EXT",
-  "TRADEREPUBLIC", "GIRO",        "TRADEREPUBLIC - GIRO",   "TRBKDEBBXXX_GIR",
-  "TRADEREPUBLIC", "DEPOT",       "TRADEREPUBLIC - DEPOT",  "TRBKDEBBXXX_DEP",
-  "FLATEX",        "DEPOT",       "FLATEX - DEPOT",         "BIWBDE33XXX_DEP",
-  "FLATEX",        "GUTHABEN",    "FLATEX - GUTHABEN",      "BIWBDE33XXX_GTH"
-)
-
-
-wertpapiere <- c("iShares Core S&P 500"    = "IE00B5BMR087",
-                 "Amundi Stoxx Europe 600" = "LU0908500753")
-
-wertpapiere_df <- data.frame(
-  ISIN = wertpapiere,
-  Name = names(wertpapiere)
-)
-
-# Function to load financial account data
-
-# Initialize the finanzkonto list (this will store all accounts)
-finanzkonto <- list()
-
+# funciton to load csv files
 load_finanzkonten <- function(finanzkonto) {
-  file_path <- paste0("./DATA/", finanzkonto, ".csv")
+  file_path <- paste0("./DATA/", finanzkonto)
   if (!file.exists(file_path)) {
     message("File does not exist:", file_path)
     return(NULL)  # Return NULL if the file does not exist
   }
-  read.csv(file_path)
+  read.csv(file_path, comment.char = "#")
 }
 
+# Initialize the finanzkonto list (this will store all accounts)
+bank_konto <- data.frame()
+finanzkonto <- list()
+
 # Loop through each account and load the data dynamically
-for(i in 1:nrow(bank_konto)) {
-  ID <- pull(bank_konto[i, "ID"])
-  #options(show.error.messages = FALSE)
+for(i in filenames_csv) {
+  
+  info  <- readLines(paste0("./DATA/", i))
+  bank  <- trimws(gsub(".*:", "", info[1]))
+  konto <- trimws(gsub(".*:", "", info[2]))
+  display_name  <- paste0(bank, " - ", konto)
+  
   tryCatch({
     # Load the data for the current ID
-    account_data <- load_finanzkonten(ID)
-    
+    account_data <- load_finanzkonten(i)
+    ID <- gsub("*.csv", "", i)
     # Only add to finanzkonto if the data is not NULL
     if (!is.null(account_data)) {
       finanzkonto[[ID]] <- list(
         name = ID,  
-        display_name = pull(bank_konto[i, "name"]),
+        display_name = display_name,
         data = account_data
       )
     } else {
@@ -84,8 +68,11 @@ for(i in 1:nrow(bank_konto)) {
     message(paste("Error loading data for", ID, ":", e$message))
   })
   #options(show.error.messages = TRUE)
+  
+  konto <- data.frame(bank = bank, konto = konto, name = display_name, ID = ID)
+  bank_konto <- bind_rows(konto, bank_konto)
+  
 }
-
 
 
 ################################################################################
